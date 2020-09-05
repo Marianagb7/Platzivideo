ffcc1e36661349d8a158d875b77a94c5a3bfe6d3
Mariana Gomez <marisgob777@hotmail.com>
2020-09-05 16:15:24 -0500

    Changes2

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,16 @@
+import React, { Children } from 'react';
+import '../assets/styles/components/Carousel.scss';
+
+
+const Carousel = ({ Children }) => (
+    <section className="carousel">
+    <div className="carousel__container">
+        { Children }
+    </div>
+    </section>
+    
+
+);
+
+
+export default Carousel;
